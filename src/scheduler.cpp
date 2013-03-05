64d937a127fe52ab140c80c29b6328af3655878e
Daniel Bugl <daniel.bugl@touchlay.com>
2013-03-05 15:56:18 +0100

    Added scheduler

---
+++ src/scheduler.cpp
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  scheduler.cpp
+ *
+ *    Description:  TouchNet 2 Scheduler
+ *
+ *        Version:  0.1
+ *        Created:  05/03/13 15:54:32
+ *       Revision:  none
+ *       Compiler:  g++
+ *
+ *         Author:  Daniel Bugl <Daniel.Bugl@touchlay.com>
+ *   Organization:  TouchLay
+ *
+ * =====================================================================================
+ */
+
+// Headers
+#include "scheduler.h"
+
+Scheduler::Scheduler() {
+ 
+}
---
+++ src/scheduler.h
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  scheduler.h
+ *
+ *    Description:  TouchNet 2 scheduler headers
+ *
+ *        Version:  0.1
+ *        Created:  05/03/13 15:55:37
+ *       Revision:  none
+ *       Compiler:  g++
+ *
+ *         Author:  Daniel Bugl <Daniel.Bugl@touchlay.com>
+ *   Organization:  TouchLay
+ *
+ * =====================================================================================
+ */
+
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
+
+#include "config.h"
+
+// Standard headers
+#include <iostream>
+
+class Scheduler {
+public:
+ Scheduler();
+};
+
+#endif /* SCHEDULER_H */
